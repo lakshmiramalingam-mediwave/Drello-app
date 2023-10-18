1233c4e98a0629a0f0001ba942d422ae04a0d17b
lakshmiramalingam-mediwave <lakshmi_ramalingam@mindwaveventures.com>
2023-10-18 19:18:49 +0530

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,72 @@
+import { useReducer } from "react";
+import Card from "./components/Card";
+import { v4 as uuidv4 } from "uuid";
+import "./App.css";
+
+function App() {
+  const [tasks, dispatch] = useReducer(todoReducer, []);
+
+  function todoReducer(tasks, action) {
+    switch (action.type) {
+      case "TODO_ADD": {
+        return [
+          ...tasks,
+          {
+            id: uuidv4(),
+            text: "",
+            dateTime: new Date(),
+            inState: "todo",
+          },
+        ];
+      }
+      case "TODO_EDITED": {
+        const editedTask = [...tasks];
+        const idx = editedTask.findIndex((nt) => nt.id === action.value.id);
+        if (idx !== -1) {
+          editedTask[idx]["text"] = action.value.value;
+        }
+        return editedTask;
+      }
+      default: {
+        throw Error("Unknown action: " + action.type);
+      }
+    }
+  }
+
+  function handleAdd(value) {
+    dispatch({
+      type: "TODO_ADD",
+      value: value,
+    });
+  }
+  function handleEdited(value, id) {
+    dispatch({
+      type: "TODO_EDITED",
+      value: { value, id },
+    });
+  }
+
+  return (
+    <div class="totaldiv">
+      <h1>DRELLO </h1>
+      <div className="total-div">
+        <div className="container">
+          <h2>My todo</h2>
+          <Card
+            addTodo={(text) => handleAdd(text)}
+            tasks={tasks}
+            edited={handleEdited}
+          />
+        </div>
+        <div className="progess">
+          <h2>progress</h2>
+        </div>
+        <div className="done">
+          <h2>Done</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/Card.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { formatDate } from "../utils";
+function Card({ addTodo, edited, tasks }) {
+  const [text, setText] = useState("");
+
+  const addCard = () => {
+    addTodo(text);
+  };
+  function handleBlockquoteChange(newValue) {
+    setText(newValue);
+  }
+
+  return (
+    <>
+      <button onClick={addCard}>+</button>
+      <div className="">
+        {tasks.map((todo, id) => (
+          <div key={id}>
+            <textarea
+              key={id}
+              onChange={(e) => handleBlockquoteChange(e.target.value)}
+              onBlur={(e) => edited(e.target.value, todo.id)}
+              placeholder="enter"
+            />
+            <div>{formatDate(todo.dateTime)}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Card;
